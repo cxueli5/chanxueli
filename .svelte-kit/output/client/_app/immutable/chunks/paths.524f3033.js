5f1796ea8aa0964b217907374a080314c90896a7
Chan Xue Li <2717950C@student.gla.ac.uk>
2024-02-09 12:53:01 +0800

    test favicon

--- .svelte-kit/output/client/_app/immutable/chunks/paths.524f3033.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_ch6as8)==null?void 0:s.base)??"/chanxueli";var a;const t=((a=globalThis.__sveltekit_ch6as8)==null?void 0:a.assets)??e;export{t as a,e as b};
--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.57328514.js","_app/immutable/chunks/scheduler.61ee4713.js","_app/immutable/chunks/index.ad37b216.js","_app/immutable/chunks/paths.524f3033.js"];
+export const imports = ["_app/immutable/nodes/2.6f96c3a4.js","_app/immutable/chunks/scheduler.61ee4713.js","_app/immutable/chunks/index.ad37b216.js","_app/immutable/chunks/paths.8d7fb80d.js"];
 export const stylesheets = [];
 export const fonts = [];
