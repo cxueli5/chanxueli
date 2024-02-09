c63f238921d4616e520e4a29b357be533d99003e
Chan Xue Li <2717950C@student.gla.ac.uk>
2024-02-09 12:44:08 +0800

    test error page

---
+++ .svelte-kit/output/client/_app/immutable/chunks/paths.ecd64b87.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_305qt9)==null?void 0:s.base)??"/chanxueli";var t;const e=((t=globalThis.__sveltekit_305qt9)==null?void 0:t.assets)??a;export{e as a,a as b};
--- .svelte-kit/output/server/nodes/3.js
+++ .svelte-kit/output/server/nodes/3.js
@@ -3,6 +3,6 @@
 export const index = 3;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/about-me/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.360d5b92.js","_app/immutable/chunks/scheduler.61ee4713.js","_app/immutable/chunks/index.ad37b216.js","_app/immutable/chunks/paths.d022023b.js","_app/immutable/chunks/Step.b8234421.js"];
+export const imports = ["_app/immutable/nodes/3.6cb131ed.js","_app/immutable/chunks/scheduler.61ee4713.js","_app/immutable/chunks/index.ad37b216.js","_app/immutable/chunks/paths.ecd64b87.js","_app/immutable/chunks/Step.b8234421.js"];
 export const stylesheets = ["_app/immutable/assets/3.27605dcc.css","_app/immutable/assets/Step.eb9a94c3.css"];
 export const fonts = [];
