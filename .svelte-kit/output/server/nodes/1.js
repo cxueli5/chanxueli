10430952ec885c5b44d3cc4b5163fe4eccc45236
Chan Xue Li <2717950C@student.gla.ac.uk>
2024-02-09 12:48:20 +0800

    test favicon

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.07d703aa.js","_app/immutable/chunks/scheduler.61ee4713.js","_app/immutable/chunks/index.ad37b216.js","_app/immutable/chunks/singletons.d78dd676.js","_app/immutable/chunks/paths.ecd64b87.js"];
+export const imports = ["_app/immutable/nodes/1.228d1671.js","_app/immutable/chunks/scheduler.61ee4713.js","_app/immutable/chunks/index.ad37b216.js","_app/immutable/chunks/singletons.e830725f.js","_app/immutable/chunks/paths.524f3033.js"];
 export const stylesheets = [];
 export const fonts = [];
